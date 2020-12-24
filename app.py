6f8e9374db3394dd01894c6ff733bff03ce8107a
KoenKieft <koen.kieft@student.hu.nl>
2020-12-24 13:49:37 +0100

    stap 7

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import os
 from flask import Flask
 from buzz import generator
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
+    page += "Greetings from <br> Koen Kieft</br>"
     return page
 
 if __name__ == "__main__":
